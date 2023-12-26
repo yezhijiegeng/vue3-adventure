5d72d207c67094fc0efc9d6ae84a485399e4dc67
yezhijiegeng <y2474498183@sina.com>
2023-12-26 08:23:04 +0800

    fix: 输入bug

--- src/views/ai/chat/index.vue
+++ src/views/ai/chat/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="ai-chat">
-  
+
     <el-card>
       <div v-for="(item, index) in messages"
            :key="index">
@@ -11,45 +11,67 @@
       </div>
     </el-card>
     <el-card class="opt-card">
-      <el-input v-model="message"
+      <el-input v-model="inputMessage"
                 placeholder="请输入内容"
                 clearable
                 @keydown.enter.native="sendMessage"></el-input>
       <el-button type="primary"
-                 @click="sendMessage" class="send-btn">发送</el-button>
+                 @click="sendMessage"
+                 class="send-btn">发送</el-button>
     </el-card>
   </div>
 </template>
 <script lang="ts">
 import { Options, Ref, Vue } from "vue-property-decorator";
+import axios from "axios";
+import { tr } from "element-plus/es/locale";
+import { log } from "console";
 
 @Options({
-  name:'AiChat',
-  components:{
-  }
+  name: "AiChat",
+  components: {},
 })
 export default class AiChat extends Vue {
-  // @Ref("inputElement") 
-	inputElement!: HTMLInputElement;
-
-  message = "";
+  // @Ref("inputElement")
+  inputElement!: HTMLInputElement;
   messages = [
     { user: "user", content: "你好" },
     { user: "ai", content: "你好，有什么我可以帮助你的？" },
   ];
+  inputMessage:string = "";
 
-  sendMessage() {
-    if (this.message.trim() !== "") {
-      this.messages.push({ user: "user", content: this.message });
-      this.message = "";
-      // 调用 AI 聊天 API 或模型
+  async sendMessage() {
+    if (this.inputMessage .trim() !== "") {
+      this.messages.push({ user: "user", content: this.inputMessage });
+
+      try{
+        
+      // 调用 AI 接口，获取回复
+      const response = await axios.post(
+        "https://api.qianwenguan.com/chat",
+        {
+          question: this.inputMessage,
+        }
+      );
+
+      const answer = response.data.answer;
+      this.messages.push({ author: "AI", text: answer });
+      
+      this.inputMessage = "";
+
+      } catch(e){
+        this.inputMessage = "";
+        console.log(e)
+      }
+      
+      
     }
   }
 }
 </script>
 <style lang="scss" scoped >
 .ai-chat {
-/*   display: flex;
+  /*   display: flex;
   justify-content: center;
   align-items: center;
   height: 100vh; */
