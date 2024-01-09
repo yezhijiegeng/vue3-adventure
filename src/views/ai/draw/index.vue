ca7d3523dea3f5611be9ec7d5f74f14465e39775
yezhijiegeng <y2474498183@sina.com>
2024-01-09 13:03:53 +0800

    绘图子页面

--- src/views/ai/draw/index.vue
+++ src/views/ai/draw/index.vue
@@ -1,17 +1,25 @@
 <template>
-	<div>
-		画画
-	</div>
+  <div>
+    <el-tabs v-model="activeName" class="demo-tabs" @tab-click="handleClick">
+      <el-tab-pane label="模型" name="model"></el-tab-pane>
+      <el-tab-pane label="绘图创作" name="drawCreate"></el-tab-pane>
+    </el-tabs>
+  </div>
 </template>
 
 <script lang="ts">
 import { Options, Ref, Vue } from "vue-property-decorator";
+import type { TabsPaneContext } from 'element-plus'
 
 @Options({
   name: "AiHome",
   components: {},
 })
 export default class AiDraw extends Vue {
-	
+  activeName = "model";
+
+  handleClick = (tab: TabsPaneContext, event: Event) => {
+    console.log(tab, event);
+  };
 }
 </script>
